ac6ec8364ede789423fc2d2529f597a06e2ddee1
Anders Janmyr <anders@janmyr.com>
2012-11-10 21:29:39 +0100

    Added test and file

---
+++ test/parser_spec.js
@@ -0,0 +1,21 @@
+"use strict";
+
+require('mocha');
+var expect = require('chai').expect;
+
+var Parser = require('parser');
+
+describe('Parser', function() {
+  var parser;
+ 
+  beforeEach(function() {
+    parser = new Parser();
+  });
+
+  describe('#parse', function() {
+    it('does something useful', function() {
+      parser.parse();
+    });
+  });
+});
+
