b2492be400fcaf750a22a6e9cd67920cde6b7164
Anders Janmyr <anders@janmyr.com>
2012-11-12 14:18:27 +0100

    Added options parsing

--- test/main_spec.js
+++ test/main_spec.js
@@ -8,9 +8,21 @@ var main = require('../lib/main');
 
 describe('main', function() {
   describe('#files', function() {
-    it('returns 8 files', function() {
+    it('returns 5 nested files for fixtures/**/*.js', function() {
       var files = main.files(__dirname + '/fixtures/**/*.js', {debug: true});
       expect(files).to.have.length(5);
+    });
+  });
+  describe('#parseArgs', function() {
+    it('parses valid options', function() {
+      var options = main.parseArgs(['node', 'main', '-t', 'libtags', '**/*.js']);
+      expect(options.tagfile).to.equal('libtags');
+      expect(options.pattern).to.deep.equal('**/*.js');
+    });
+    it('inserts default options', function() {
+      var options = main.parseArgs(['node', 'main']);
+      expect(options.tagfile).to.equal('tags');
+      expect(options.pattern).to.deep.equal('*.js');
     });
   });
 });
