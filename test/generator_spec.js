01a1398463967b54e13db6f08f316b8b2113e6b7
Anders Janmyr <anders@janmyr.com>
2012-11-12 17:08:02 +0100

    Refactored

--- test/generator_spec.js
+++ test/generator_spec.js
@@ -3,15 +3,14 @@
 require('mocha');
 var expect = require('chai').expect;
 
-var Generator = require('../lib/generator');
+var generate = require('../lib/generator');
 
 
 describe('Generator', function() {
-    var generator = new Generator();
-    describe('#generate', function() {
+    describe('#generateFile', function() {
         var entries;
         beforeEach(function() {
-            entries = generator.generate('file.js', [{name: 'tapir', line: 5 }]);
+            entries = generate.generateFile('file.js', [{name: 'tapir', line: 5 }]);
         });
         it('generates one ctag entry', function() {
             expect(entries).to.have.length(1);
