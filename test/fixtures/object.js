632775d85e56c03717c0fe404534407ead5cd2a5
Anders Janmyr <anders@janmyr.com>
2012-11-12 10:59:31 +0100

    added parsing of multiple properties

---
+++ test/fixtures/object.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var Module = {
+  internalMethod: function() {}
+};
+
+Module.externalMethod = function() {};
+
+Module.prototype.prototypeMethod = function() {};
+
+function namedFunction() {}
+
+
--- test/parser_spec.js
+++ test/parser_spec.js
@@ -2,19 +2,48 @@
 
 require('mocha');
 var expect = require('chai').expect;
+var fs = require('fs');
+var _ = require('underscore');
 
 var Parser = require('../lib/parser');
 
+var jQueryFixture = fs.readFileSync(__dirname + '/fixtures/jQuery-1.8.2.js', 'utf-8');
+var objectFixture = fs.readFileSync(__dirname + '/fixtures/object.js', 'utf-8');
+
 describe('Parser', function() {
   var parser;
- 
+
   beforeEach(function() {
     parser = new Parser();
   });
 
-  describe('#parse', function() {
-    it('does something useful', function() {
-      parser.parse();
+  describe('#parse jQuery', function() {
+    var tree;
+    beforeEach(function() {
+      //tree = parser.parse(jQueryFixture);
+    });
+    xit('succeeds', function() {
+      expect(tree).to.be.defined;
+    });
+  });
+
+  describe('#parse object literals', function() {
+    var list;
+    var names;
+    beforeEach(function() {
+      list = parser.parse(objectFixture);
+      names = _.pluck(list, 'name');
+      console.log(list, names);
+    });
+
+    it('parses named functions', function() {
+      expect(names).to.contain('namedFunction');
+    });
+    it('parses external methods', function() {
+      expect(names).to.contain('externalMethod');
+    });
+    it('parses prototype methods', function() {
+      expect(names).to.contain('prototypeMethod');
     });
   });
 });
